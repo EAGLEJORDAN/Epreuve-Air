5bb6cb555db40c7996bcfef3a789ab004b123a72
EAGLEJORDAN <114489625+EAGLEJORDAN@users.noreply.github.com>
2023-01-20 11:17:11 +0100

    Add files via upload

---
+++ air14.js
@@ -0,0 +1,10 @@
+let argt=process.argv.slice(2);
+if (argt[0]!=undefined)
+{
+    console.log("error");
+}
+else if(argt[0]===undefined)
+{
+    console.log(`Epreuve de l'air très enrichissante, j'ai découvert d'autres boîtes à outil et leurs outils. 
+Les algorithmes de trie sont géniaux! Merci Yann Lescot pour la correction!`)
+}
